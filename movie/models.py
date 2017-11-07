0ec06121a7e40c6383e8c7716e80651d3ee9dc16
harishVerma <harishverma555@gmail.com>
2017-11-07 13:09:41 +0530

    add a messages

--- movie/models.py
+++ movie/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # Create your models here.
 # make movie models
 
+#This is movie table
 class Movie(models.Model):
 	actor = models.CharField(max_length=30)
 	actor_movies = models.CharField(max_length=50)
@@ -14,15 +15,15 @@ class Movie(models.Model):
 	def __str__(self):
 		return self.actor
 
-
+#this is songs table 
 class Song(models.Model):
-	movie = models.ForeignKey(Movie, on_delete =models.CASCADE)
+	movie = models.ForeignKey(Movie, on_delete =models.CASCADE) #add ForeignKey  
 	file_type = models.CharField(max_length=50)
 	song_name = models.CharField(max_length=100)
 
 	def __str__(self):
 		return self.song_name
-
+#this is a singer tabls
 class Singer(models.Model):
 	movie = models.ForeignKey(Movie, on_delete =models.CASCADE)
 	song = models.ForeignKey(Song, on_delete =models.CASCADE)
